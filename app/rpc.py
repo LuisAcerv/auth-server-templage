23f098addf2941ce3e64317c0dad2cebaaf367ff
luis acerv <l.arr.cerv@gmail.com>
2019-10-30 22:22:10 -0600

    use environment variables

--- app/rpc.py
+++ app/rpc.py
@@ -1,6 +1,14 @@
+import os
+
 from Savoir import Savoir
+from dotenv import load_dotenv
+from pathlib import Path 
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
 rpcuser='multichainrpc'
-rpcpassword='342oaD9VTQ6CaJRpGNnWDTAxrsEvWExLF1TZJhBS9S3n'
+rpcpassword=os.getenv('rpcpassword')
 rpchost = '172.17.0.2'
 rpcport = '4346'
 chainname = 'mainchain-dev'
